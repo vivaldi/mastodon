184da17cab903303f13d0ceab135a0df7b350b76
Ísak Garðarsson <isak@vivaldi.com>
2022-11-10 12:54:15 +0000

    Vivaldi branding changes

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,7 +1,7 @@
 import logo from 'mastodon/../images/logo.svg';
 
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
+  <svg viewBox='0 0 196 48' className='logo logo--wordmark' role='img'>
     <title>Mastodon</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
