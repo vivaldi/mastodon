3cd5c70fe6736244c616b9f23e2518b79b557f0c
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2023-08-06 18:16:58 +0000

    customize version and source URL

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -17,7 +17,7 @@ module Mastodon
     end
 
     def default_prerelease
-      ''
+      'vivaldimod'
     end
 
     def prerelease
@@ -44,7 +44,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'vivaldi/mastodon')
     end
 
     def source_base_url
@@ -53,7 +53,7 @@ module Mastodon
 
     # specify git tag or commit hash here
     def source_tag
-      ENV.fetch('SOURCE_TAG', nil)
+      ENV.fetch('SOURCE_TAG', "v#{self}")
     end
 
     def source_url
